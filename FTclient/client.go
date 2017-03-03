51989d5d875c83a4d3aa575c20e872e2d9ef0290
God Kra <God Kra>
2017-03-03 15:23:28 +0500

    Added directory transfering to client. Fixed bugs

--- FTclient/client.go
+++ FTclient/client.go
@@ -1,49 +1,101 @@
 package main
 
 import (
-	"net"
-	"log"
+	"encoding/binary"
+	"flag"
 	"fmt"
-    "os"
-    "io"
-    "encoding/binary"
+	"github.com/jhoonb/archivex"
+	"io"
+	"log"
+	"net"
+	"os"
+)
+
+var (
+	path   = flag.String("path", "FileTransfer", "Usage : -path <path> eg : -path FileTransfer/test")
+	ipFlag = flag.String("ip", "localhost:5555", "Usage : -ip <ip address:port> eg : -ip localhost:5555")
 )
 
 func main() {
-    args := os.Args
-    args = args[1:]
-    file := args[0]
-    ip := args[1]
-    f, e := os.Open(file)
-    checkError(e)
+	flag.Parse()
 
-    conn := connect(ip)
-    stat, e := f.Stat()
-    checkError(e)
-    conn.Write([]byte(stat.Name() + "\n"))
-    sendFileSize(stat.Size(), conn)
+	s, e := os.Stat(*path)
+	checkError(e)
+	ip := *ipFlag
+	name := "temp"
 
-    i, e := io.Copy(conn, f)
-    defer checkError(e)
-    fmt.Println("Sending...")
-    fmt.Printf("Succesfully sent %v bytes of data\n", i)
-    conn.Close()
+	var z zipper
+	if s.IsDir() {
+		z = dir(*path)
+	} else {
+		z = file(*path)
+	}
+	z.zip(name, *path)
+
+	f, e := os.Open(name + ".zip")
+	defer f.Close()
+	checkError(e)
+
+	defer os.Remove(name + ".zip")
+
+	conn := connect(ip)
+	stat, e := f.Stat()
+	checkError(e)
+
+	e = sendFileSize(stat.Size(), conn)
+	checkError(e)
+
+	fmt.Println(stat.Size())
+	i, e := io.Copy(conn, f)
+	checkError(e)
+
+	fmt.Println("Sending...")
+
+	fmt.Printf("Succesfully sent %v bytes of data\n", i)
+	conn.Close()
 }
 
-func connect(ip string) net.Conn{
-    conn, e := net.Dial("tcp", ip)
-    checkError(e)
-    return conn
+func connect(ip string) net.Conn {
+	conn, e := net.Dial("tcp", ip)
+	checkError(e)
+	return conn
 }
 
 func checkError(e error) {
-    if e != nil {
-        log.Fatal(e)
-    }
-} 
+	if e != nil {
+		log.Fatal(e)
+	}
+}
 
-func sendFileSize(size int64, conn net.Conn) {
-    b := [8]byte{}
-    binary.BigEndian.PutUint64(b[:], uint64(size))
-    conn.Write(b[:])
+func sendFileSize(size int64, conn net.Conn) error {
+	b := [8]byte{}
+	binary.BigEndian.PutUint64(b[:], uint64(size))
+	_, e := conn.Write(b[:])
+	return e
+}
+
+type zipper interface {
+	zip(name, path string)
+}
+
+type dir string
+
+func (d dir) zip(name, path string) {
+	zipfile := archivex.ZipFile{}
+	e := zipfile.Create(name)
+	checkError(e)
+	e = zipfile.AddAll(path, false)
+	checkError(e)
+	zipfile.Close()
+}
+
+type file string
+
+func (f file) zip(name string, path string) {
+	zipfile := archivex.ZipFile{}
+	e := zipfile.Create(name)
+	checkError(e)
+	e = zipfile.AddFile(path)
+	checkError(e)
+	zipfile.Close()
 }
--- FTserver/server.go
+++ FTserver/server.go
@@ -1,52 +1,48 @@
 package main
 
 import (
-	"net"
-	"log"
-	"os"
-	"io"
-	"fmt"
-	"encoding/binary"
 	"bufio"
-	"strings"
+	"encoding/binary"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"os"
+	
 )
 
 func main() {
-    l, e := listenServer("localhost:5555")
-    checkError(e)
-    conn, e := l.Accept()
+	name := os.Args[1]
+	l, e := listenServer(":5555")
 	checkError(e)
-	name := getName(conn)
-	size := setSize(conn)
+	conn, e := l.Accept()
+	checkError(e)
+	bufcon := bufio.NewReader(conn)
+	checkError(e)
+	size, e := getSize(bufcon)
+	checkError(e)
 	os.Mkdir("FileTransfer", 0666)
 	os.Chdir("FileTransfer")
-    f, _ := os.Create(name)
-	fmt.Printf("Copying file( %v ) from %v\n", name, conn.RemoteAddr())
-    copyBuffer(f, conn, size)
-    checkError(e)
+	f, _ := os.Create(name + ".zip")
+	fmt.Printf("Copying file(%v) from %v\n", name, conn.RemoteAddr())
+	copyBuffer(f, conn, size)
+	checkError(e)
 }
 
 func checkError(e error) {
 	if e != nil {
-        log.Fatal(e)
-    }
+		log.Fatal(e)
+	}
 }
 
-func setSize(conn net.Conn) uint64{
+func getSize(conn io.Reader) (size uint64, e error) {
 	b := [8]byte{}
-	conn.Read(b[:])
-	size := binary.BigEndian.Uint64(b[:])
-	return size
+	_, e = conn.Read(b[:])
+	size = binary.BigEndian.Uint64(b[:])
+	return
 }
 
-func getName(conn net.Conn) (name string) {
-	r := bufio.NewReader(conn)
-	b, _ := r.ReadBytes('\n')
-	name = strings.TrimSpace(string(b[:]))
-	return name
-}
-
-func listenServer(addr string) (listener net.Listener, e error){
+func listenServer(addr string) (listener net.Listener, e error) {
 	listener, e = net.Listen("tcp", addr)
 	checkError(e)
 	return
@@ -54,13 +50,13 @@ func listenServer(addr string) (listener net.Listener, e error){
 
 func copyBuffer(dst io.Writer, src io.Reader, size uint64) {
 	buf := make([]byte, 32*1024)
-    var written int
+	var written int
 	for {
 		nr, er := src.Read(buf)
 		if nr > 0 {
 			nw, ew := dst.Write(buf[0:nr])
-            written += nw
-            fmt.Printf("\r%v of %v", written, size)
+			written += nw
+			fmt.Printf("\r%v of %v bytes", written, size)
 			checkError(ew)
 			if nr != nw {
 				log.Fatal(io.ErrShortWrite)
